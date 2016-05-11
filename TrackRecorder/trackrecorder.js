cf30dc634252ba02af8f17311c3fb357c1c6b68d
Guillaume Prêcheur <gpr@magellium.fr>
2016-05-11 21:46:20 +0200

    Mise en base (followme/testmessages) des messages reçus sur dgidgi.followme.trackrecorder

--- TrackRecorder/trackrecorder.js
+++ TrackRecorder/trackrecorder.js
@@ -1,12 +1,38 @@
+// trackrecorder.js
+var debug = require('debug')('trackrecorder')
 var mqtt    = require('mqtt');
-var client  = mqtt.connect('mqtt://test.mosquitto.org');
+var mqttClient  = mqtt.connect('mqtt://test.mosquitto.org');
+var mongoClient = require('mongodb').MongoClient ;
 
-client.on('connect', function () {
-  client.subscribe('dgidgi.followme.trackrecorder');
-});
+var mongoDbUrl = 'mongodb://localhost:27017/followme'
 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString());
-  
+// Connexion à la base
+mongoClient.connect( mongoDbUrl, function(err, mongodb) {
+    if(err) throw err;
+    debug('mongodb connection to'+ mongoDbUrl +' : ok') ;
+    debug('mongodb') ;
+
+    // Connection Mqtt et subsciption
+    mqttClient.on('connect', function () {
+      mqttClient.subscribe('dgidgi.followme.trackrecorder');
+    });
+
+    // Reception d'un message
+    mqttClient.on('message', function (topic, message) {
+        debug('msg received') ;
+        debug('topic:') ;
+        debug(topic) ;
+        debug('msg:') ;
+        debug(message.toString()) ;
+
+        var doc= {  message: message.toString() };
+
+        mongodb.collection('testmessages').insert(doc, function (err, data) {
+                if(err) throw err;
+
+                debug('message added in database :') ;
+                debug(data) ;
+        });
+    });
+
 });
