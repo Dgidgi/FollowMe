a10e87aed4bd010c3c7409181ed38a3ef18cf3bf
Guillaume Prêcheur <gpr@magellium.fr>
2016-05-20 22:31:14 +0200

    Refactoring et calcul des distances avec les autres tracks

--- Server/Sources/trackrecorder.js
+++ Server/Sources/trackrecorder.js
@@ -17,6 +17,12 @@ function findPreviousLocationSample( appId) {
  return mongoDbConnection.collection('tracking').find({applicationId:appId}).sort({'loc.time':-1}).limit(1) ;
 }
 
+// Retourne le dernier sample appId avec une distance non null
+///////////////////////////////////////////////////////////////////////////////
+function findLastLocationSampleWithDistance( appId) {
+ return mongoDbConnection.collection('tracking').find({applicationId:appId,distance:{$exists:true}}).sort({'loc.time':-1}).limit(1) ;
+}
+
 // Demande de status
 // Retourne le dernier sample associé à l'appId
 ///////////////////////////////////////////////////////////////////////////////
@@ -41,20 +47,47 @@ function requestClientStatus( sourceTopic) {
 
 // Appelé lorsqu'une capture est interompue
 ///////////////////////////////////////////////////////////////////////////////
-function manageEndTrack( sourceTopic) {
-    var appId  =  extractApplicationIDFromTopic( sourceTopic) ;
+function manageEndTracking( appId) {
+
     if (appId==null || appId=="")
         return ;
 
-    debug('manageEndTrack for applicationId : ['+appId+']') ;
+    debug('manageEndTracking for applicationId : ['+appId+']') ;
 
     if ( mongoDbConnection == null )
         return ;
 
     mongoDbConnection.collection('tracking').remove( {applicationId:appId}, function (err, result) {
             if(err) throw err;
-            debug('track cleaned') ;
+            debug('tracking cleaned') ;
             debug(result) ;
+    } );
+
+    mongoDbConnection.collection('tracks').remove( {applicationId:appId}, function (err, result) {
+            if(err) throw err;
+            debug('track removed') ;
+            debug(result) ;
+    } );
+
+}
+// Appelé lorsqu'une capture est interompue
+///////////////////////////////////////////////////////////////////////////////
+function manageStartTracking( appId) {
+
+    if (appId==null || appId=="")
+        return ;
+
+    debug('manageStartTracking for applicationId : ['+appId+']') ;
+
+    if ( mongoDbConnection == null )
+        return ;
+
+    var track={applicationId:appId} ;
+    // Stockage en base de la donnée
+    mongoDbConnection.collection('tracks').insert(track, function (err, data) {
+        if(err) throw err;
+        debug('track added in db :') ;
+        debug(JSON.stringify(data)) ;
     } );
 }
 
@@ -73,23 +106,19 @@ function extractApplicationIDFromTopic( sourceTopic) {
     return splited[splited.length -1] ;
 }
 
+
 //
-// Ajout d'un nouveau sample sur la track
+// Mise à jour du sample status
 ///////////////////////////////////////////////////////////////////////////////
-function manageAddTrackSample( lastSample ) {
-
-    debug('manageAddTrackSample ['+JSON.stringify(lastSample)+']') ;
-
-    // TODO Tester la confirmité du message
-    if ( mongoDbConnection == null)
-        return ;
+function updateTrackSampleStatus( lastSample, applicationId ) {
 
     // Recherche du dernier sample acquis sur le même track
-    var cursor = findPreviousLocationSample(lastSample.applicationId) ;
-
+    var cursor = findPreviousLocationSample(applicationId) ;
 
     lastSample.distance = 0.0 ;
     cursor.nextObject(function(err, prevSample) {
+
+        // Calcul de la distance parcourue
         debug('found item :['+JSON.stringify(prevSample)+']') ;
 
         if ( prevSample != null) {
@@ -104,6 +133,30 @@ function manageAddTrackSample( lastSample ) {
             debug(' current distance '+prevSample.distance+' -> '+lastSample.distance) ;
 	    }
 
+        // Calcul des delta distance
+
+        // Recherche des autres track en cours
+        var others = mongoDbConnection.collection('tracks').find({applicationId:{$ne:applicationId}}) ;
+
+        lastSample.delta = [] ;
+
+        others.nextObject(function(err, other) {
+            debug('found other :['+JSON.stringify(other)+']') ;
+            if (other != null) {
+                var lastLocationSample = findLastLocationSampleWithDistance(other.applicationId) ;
+                lastLocationSample.nextObject(function(err, other) {
+
+                    lastSample.delta.push( {applicationId:other.applicationId, distance:  lastSample['distance'] - other.distance  });
+
+                    debug('lastSample withi status :['+JSON.stringify(lastSample)+']') ;
+                });
+            }
+        });
+
+        lastSample.delta.push( {applicationId:'test-application-id-1', distance:  lastSample['distance']   });
+        lastSample.delta.push( {applicationId:'test-application-id-2', distance:  lastSample['distance'] -10    });
+
+
         debug('object to save :['+JSON.stringify(lastSample)+']') ;
 
         // Stockage en base de la donnée
@@ -113,6 +166,19 @@ function manageAddTrackSample( lastSample ) {
             debug(JSON.stringify(data)) ;
         } );
     } );
+}
+//
+// Ajout d'un nouveau sample sur la track
+///////////////////////////////////////////////////////////////////////////////
+function manageAddTrackSample( lastSample ) {
+
+    debug('manageAddTrackSample ['+JSON.stringify(lastSample)+']') ;
+
+    // TODO Tester la confirmité du message
+    if ( mongoDbConnection == null)
+        return ;
+
+    updateTrackSampleStatus( lastSample, lastSample.applicationId ) ;
 }
 
 //
@@ -144,7 +210,12 @@ mongoClient.connect( mongoDbUrl, function(err, mongodb) {
 
         if ( message.toString().indexOf("endtrack") != -1) {
             // Gestion en fin de tracking
-            manageEndTrack( topic) ;
+            manageEndTracking(  extractApplicationIDFromTopic( topic) ) ;
+            return ;
+
+        } else if ( message.toString().indexOf("starttrack") != -1) {
+            // Gestion du début tracking
+            manageStartTracking(  extractApplicationIDFromTopic( topic) ) ;
             return ;
 
         } else if ( message.toString().indexOf("status") != -1) {
